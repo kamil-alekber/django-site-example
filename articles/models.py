6546f38866420bf34df5d8fa8c7f5c6198217f3e
kalekberov <kamil.alekberov@ps.kz>
2020-04-10 20:11:08 +0600

    feat: make migration

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# inherit from models.model
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # add thumbnail
+    # author
--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# register apps to make migration upon
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles'
 ]
 
 MIDDLEWARE = [
