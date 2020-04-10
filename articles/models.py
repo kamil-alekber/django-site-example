67d5733fa5e29614be13350e8808549fe9d6ab76
kalekberov <kamil.alekberov@ps.kz>
2020-04-10 21:54:16 +0600

    feat: add article to settings

--- articles/models.py
+++ articles/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 # inherit from models.model
+
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
@@ -9,3 +11,8 @@ class Article(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     # add thumbnail
     # author
+
+    # instead of Article: object Article shows title
+    # and it changed it in the admin section
+    def __str__(self):
+        return self.title
