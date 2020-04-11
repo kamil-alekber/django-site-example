1ca23c5d92960602b0cb470132d336cd2135ff3c
kalekberov <kamil.alekberov@ps.kz>
2020-04-11 20:31:14 +0600

    feat: url param

--- articles/urls.py
+++ articles/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.article_list),
+    path('<slug:slug>', views.article),
 ]
+# converter type int:month
+#                       <Converter Type: Query Param name>
+#   path('articles/<int:year>/<int:month>/<slug:slug>/', views.article_detail),
--- articles/views.py
+++ articles/views.py
@@ -5,3 +5,9 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+
+def article(request, slug):
+    print(slug, 'slug')
+    # articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/article.html')
