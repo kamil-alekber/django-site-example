c573b117676181b9cf9165416a06e66ee1c7fbe0
kalekberov <kamil.alekberov@ps.kz>
2020-04-12 16:12:43 +0600

    feat: loggin in users

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 # from .models import Article
 
 
@@ -8,10 +9,24 @@ def signup_view(request):
         # check the form validation
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             # log the user in
-            # appName:approute name
+            # appName:route name
             return redirect('articles:article_list')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log in the user
+            user = form.get_user()
+            login(request, user)
+            return redirect('articles:article_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
